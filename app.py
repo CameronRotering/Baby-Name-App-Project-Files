74c9ca9c602b86e4bc39d700a7aef3aad7a3f9fe
CameronRotering <118744051+CameronRotering@users.noreply.github.com>
2024-05-10 10:31:17 -0400

    Read in only sex, year, name, and count. Constructed a column named rank in year.

--- app.py
+++ app.py
@@ -66,14 +66,17 @@ def get_name_popularity():
 ### Main -- runs when the app starts
 
 # Load babynames.csv into a pandas dataframe using the path './data/babynames.csv'
-# Keep only the columns 'sex', 'year', 'name' and 'count
+# Name the columns 'sex', 'year', 'name' and 'count'
 # Name this dataframe "babynames"
 
+babyNames = pd.read_csv("./data/babynames.csv", names = ["sex", "year", "name", "count"]) # Count is count of names within each year
 
 # Construct a column giving the rank within each year and sex for each name
 #
 # e.g. Mary is the #1 ranking name for girls in 1910
 #      John is the #1 ranking name for boys in 1910
+
+babyNames["rank_in_year"] = babyNames.groupby(["year", "sex"])["count"].rank(ascending = False)
 
 #The output should be a column of babynames called 'rank_in_year'
 #Calculate rank in year by grouping babynames by 'year' and then 'sex'
